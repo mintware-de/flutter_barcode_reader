3056ac5c2449bc87d0cf54f0d99177200f2e16c5
Matthew Smith <matthew.smith@apptreesoftware.com>
2018-01-19 04:07:31 -0800

    Add MIT license

---
+++ android/src/main/kotlin/com/yourcompany/barcodescan/BarcodeScanPlugin.kt
@@ -0,0 +1,25 @@
+package com.yourcompany.barcodescan
+
+import io.flutter.plugin.common.MethodChannel
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler
+import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.MethodCall
+import io.flutter.plugin.common.PluginRegistry.Registrar
+
+class BarcodeScanPlugin(): MethodCallHandler {
+  companion object {
+    @JvmStatic
+    fun registerWith(registrar: Registrar): Unit {
+      val channel = MethodChannel(registrar.messenger(), "barcode_scan")
+      channel.setMethodCallHandler(BarcodeScanPlugin())
+    }
+  }
+
+  override fun onMethodCall(call: MethodCall, result: Result): Unit {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android ${android.os.Build.VERSION.RELEASE}")
+    } else {
+      result.notImplemented()
+    }
+  }
+}
